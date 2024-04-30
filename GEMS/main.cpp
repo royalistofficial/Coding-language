d2d3a24255d7d759f5be2971e1b4d0da8b0ea55d
royalistofficial <mishabygaga1414@gmail.com>
2024-04-30 22:54:34 +0300

    add new functions

--- GEMS/main.cpp
+++ GEMS/main.cpp
@@ -3,7 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <ctime>
+#include <unistd.h>
+#include <time.h>
 
 const int WIDTH = 600;
 const int HEIGHT = 600;
@@ -12,6 +13,7 @@ const float SQUARE_SIZE = WIDTH / GRID_SIZE; // размер каждого кв
 
 // Цвета квадратов
 const std::vector<std::vector<float>> COLORS = {
+    {0.0f, 0.0f, 0.0f}, // черный
     {1.0f, 0.0f, 0.0f}, // красный
     {0.0f, 1.0f, 0.0f}, // зеленый
     {0.0f, 0.0f, 1.0f}, // синий
@@ -29,88 +31,170 @@ void drawSquare(int row, int col, float r, float g, float b) {
     glColor3f(0.0f, 0.0f, 0.0f); // Черный цвет
     glLineWidth(border_width); // Толщина линии
     glBegin(GL_LINE_LOOP);
-    glVertex2f(1.0f - 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
     glEnd();
 
     // Нарисовать внутреннюю часть квадрата
     glColor3f(r, g, b);
     glBegin(GL_QUADS);
-    glVertex2f(1.0f - 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
-    glVertex2f(1.0f - 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col+1) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
+    glVertex2f(-1.0f + 2.0f * (col) / GRID_SIZE, 1.0f - 2.0f * (row+1) / GRID_SIZE);
     glEnd();
 }
 
 void drawGrid() {
-	std::cout << "in" << std::endl;
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
 			drawSquare(i, j, COLORS[grid[i][j]][0], COLORS[grid[i][j]][1], COLORS[grid[i][j]][2]);
-			// if (grid[i][j] == -1){
-			// 	drawSquare(i, j, 0.0f, 0.0f, 0.0f);
-			// }else{
-			// 	drawSquare(i, j, COLORS[grid[i][j]][0], COLORS[grid[i][j]][1], COLORS[grid[i][j]][2]);
-			// }
         }
     }
-	std::cout << "out" << std::endl;
 }
 
+bool checkSequence(int row, int col) {
+   // Проверка горизонтальных линий
+    int sequenceLength = 0;
+    for (int i = row - 2; i <= row + 2; i++) {
+        if (i >= 0 && i < GRID_SIZE && abs(grid[i][col]) == abs(grid[row][col]) && grid[row][col] > 0) {
+            sequenceLength++;
+            if (sequenceLength >= 3) return true;
+        } else {
+            sequenceLength = 0;
+        }
+    }
+
+    // Проверка вертикальных линий
+    sequenceLength = 0;
+    for (int j = col - 2; j <= col + 2; j++) {
+        if (j >= 0 && j < GRID_SIZE && abs(grid[row][j]) == abs(grid[row][col]) && grid[row][col] > 0) {
+            sequenceLength++;
+            if (sequenceLength >= 3) return true;
+        } else {
+            sequenceLength = 0;
+        }
+    }
+
+    return false;
+}
+
+void destroySequence() {
+    bool sequenceFound = true;
+    while (sequenceFound) {
+        sequenceFound = false;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (checkSequence(i, j)) {
+                    grid[i][j] = -grid[i][j];
+                    sequenceFound = true;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            if (grid[i][j] < 0){
+                grid[i][j] = 0;
+            }
+        }
+    }
+}
+
+void blackBlockUp(){
+    bool sequenceBlack = true;
+    while (sequenceBlack) {
+        sequenceBlack = false;
+        for (int i = 1; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                if (grid[i][j] == 0 && grid[i-1][j] != 0){
+                    sequenceBlack = true;
+                    std::swap(grid[i][j], grid[i-1][j]);
+                }
+            }
+        }
+    }
+}
+
+void cleanBlackkBloc(){
+    bool sequenceBlack = true;
+    blackBlockUp();
+    // drawGrid();
+    // sleep(1);
+    while (sequenceBlack) {
+        sequenceBlack = false;
+        for (int i = 0; i < GRID_SIZE; i++) {
+            if (grid[0][i] == 0){
+                sequenceBlack = true;    
+                grid[0][i] = 1 + rand() % (COLORS.size() - 1);
+            }           
+        }
+    }
+}
+
+int prevRow = -1, prevCol = -1;
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        int row = static_cast<int>(ypos / SQUARE_SIZE);
+        int col = static_cast<int>(xpos / SQUARE_SIZE);
+
+        if ((prevRow != -1 && prevCol != -1) && (abs(prevRow-row) <= 1 && abs(prevCol - col) <= 1)) {
+            // свап ячеек
+            std::swap(grid[prevRow][prevCol], grid[row][col]);
+            prevRow = -1;
+            prevCol = -1;
+        }else{
+            prevRow = row;
+            prevCol = col;
+        }
+    }
+}
+
+
 int main() {
-    std::cout << "+" << std::endl;
+    
     srand(static_cast<unsigned int>(time(0)));
 
     // Заполнение сетки случайными значениями
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
-            grid[i][j] = rand() % COLORS.size();
-			std::cout << grid[i][j] << std::endl;
+            grid[i][j] = 1 + rand() % (COLORS.size() - 1);
         }
     }
-	std::cout << "+" << std::endl;
-    // Добавление пустых ячеек внизу сетки
-    // for (int j = 0; j < GRID_SIZE; ++j) {
-    //     for (int i = 0; i < GRID_SIZE / 2; ++i) {
-    //         grid[i][j] = -1;
-    //     }
-    // }
-	std::cout << "+" << std::endl;
+	
     GLFWwindow* window;
 
     if (!glfwInit())
         return -1;
 
     window = glfwCreateWindow(WIDTH, HEIGHT, "game", NULL, NULL);
-	// glViewport(0, 0, GRID_SIZE, GRID_SIZE);
+
     if (!window) {
         glfwTerminate();
         return -1;
     }
 
-	std::cout << "+" << std::endl;
-
     glfwMakeContextCurrent(window);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
 
-	std::cout << "+" << std::endl;
 
     while (!glfwWindowShouldClose(window)) {
-		std::cout << "-" << std::endl;
         glClear(GL_COLOR_BUFFER_BIT);
-		std::cout << "+" << std::endl;
-		// drawSquare(0, 0, 1.0f, 1.0f, 1.0f);
-        drawGrid();
-		std::cout << "+" << std::endl;
-        glfwSwapBuffers(window);
-		std::cout << "+" << std::endl;
-        glfwPollEvents();
-		std::cout << "-" << std::endl;
-    }
 
-	std::cout << "-" << std::endl;
+        drawGrid();
+        destroySequence();
+        cleanBlackkBloc();
+
+        glfwSwapBuffers(window);
+
+        glfwPollEvents();
+    }
 
     glfwTerminate();
     return 0;
