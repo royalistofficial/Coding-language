427673d057178ec0f091d9d05b254e8bff239cc8
royalistofficial <mishabygaga1414@gmail.com>
2024-05-20 20:45:59 +0300

    Arkanoid

--- Arkanoid/include/RenderText.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "const.hpp"
-#include "stb_easy_font.h"
-
-void stb_print_string(float x, float y, char *text, float r, float g, float b);
-
-void print_string(float dx, float x, float y, char *text, float r, float g, float b);
--- Arkanoid/include/main.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "Battlefield.hpp"
-#include "const.hpp"
-
-extern Battlefield* battlefield;
-static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-int main();
